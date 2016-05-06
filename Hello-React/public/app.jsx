577f645c6e33f96002f93607e7d6fc5e25bc6ca7
Julian Nicholls <juliannicholls29@gmail.com>
2016-05-06 18:45:52 +0100

    Add parameters to first React component

--- Hello-React/public/app.jsx
+++ Hello-React/public/app.jsx
@@ -1,4 +1,26 @@
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'you',
+      message: 'This is from the component'
+    };
+  },
+  render: function () {
+    var name    = this.props.name,
+        message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello to {name} from React Component</h1>
+        <p>{message}</p>
+      </div>
+    );
+  }
+});
+
+var firstName = "Julian";
+
 ReactDOM.render(
-  <h1>Hello via React</h1>,
+  <Greeter name={firstName} message="Updated Message"/>,
   document.getElementById("app")
 );
