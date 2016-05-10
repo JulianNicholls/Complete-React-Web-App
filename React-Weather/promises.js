e903308c7557ab31f41c383fd29ba65db450c80e
Julian Nicholls <juliannicholls29@gmail.com>
2016-05-10 15:30:11 +0100

    Add promises example.

---
+++ React-Weather/promises.js
@@ -0,0 +1,61 @@
+function getTempCallback(location, callback) {
+  callback(undefined, 78);
+  callback('City not found');
+}
+
+function getTempPromise(location) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(79);
+      reject('City not found');
+    }, 1000)
+  });
+}
+
+
+getTempCallback('Bournemouth', function (err, temp) {
+  if(err) {
+    console.error('Error: ' , err);
+  }
+  else {
+    console.log('Success: ', temp);
+  }
+});
+
+
+getTempPromise('Bournemouth').then(function (temp) {
+  console.log('Resolve: ', temp);
+}, function (err) {
+  console.error('Reject: ', err);
+});
+
+// Challenge
+
+function addPromise(a, b) {
+  return new Promise(function (resolve, reject) {
+    if(typeof a === 'number' && typeof b === 'number') {
+      resolve(a + b)
+    }
+    else {
+      reject("You must pass two numbers");
+    }
+  });
+}
+
+addPromise(23, 45).then(function (sum) {    // Good
+  console.log('Resolve: ', sum);
+}, function (err) {
+  console.error('Reject: ', err);
+});
+
+addPromise('a', 45).then(function (sum) {   // Bad a
+  console.log('Resolve: ', sum);
+}, function (err) {
+  console.error('Reject: ', err);
+});
+
+addPromise(23, 'a').then(function (sum) {   // Bad b
+  console.log('Resolve: ', sum);
+}, function (err) {
+  console.error('Reject: ', err);
+});
