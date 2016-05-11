4e8aada6e77a63811d3679f1bd6cf3bd91dd805c
Julian Nicholls <juliannicholls29@gmail.com>
2016-05-11 17:17:20 +0100

    Add foundation and jQuery
    Additions in package.json, webpack config, and app.jsx

--- Weather-Foundation/webpack.config.js
+++ Weather-Foundation/webpack.config.js
@@ -1,5 +1,20 @@
+var Webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new Webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
