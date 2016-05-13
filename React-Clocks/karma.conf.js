cf7d59367efb39902bec3420c581e525321e2e04
Julian Nicholls <juliannicholls29@gmail.com>
2016-05-13 09:37:27 +0100

    Install and configure testing framework: Karma, Mocha,  and Expect

---
+++ React-Clocks/karma.conf.js
@@ -0,0 +1,23 @@
+var webpackConfig = require('./webpack.config.js');
+
+module.exports = function (config) {
+  config.set({
+    browsers: ['Chrome'],
+    singleRun: true,
+    frameworks: ['mocha'],
+    files: ['app/tests/**/*.test.jsx'],
+    preprocessors: {
+      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
+    },
+    reporters: ['mocha'],
+    client: {
+      mocha: {
+        timeout: '5000'
+      }
+    },
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true
+    }
+  });
+}
