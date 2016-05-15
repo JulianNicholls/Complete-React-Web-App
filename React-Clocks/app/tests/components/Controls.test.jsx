3961c86293807564a01549f58304bec62319a6a4
Julian Nicholls <juliannicholls29@gmail.com>
2016-05-15 17:43:57 +0100

    Add tests for Controls render

--- React-Clocks/app/tests/components/Controls.test.jsx
+++ React-Clocks/app/tests/components/Controls.test.jsx
@@ -10,4 +10,26 @@ describe('Controls', () => {
   it('should exist', () => {
     expect(Controls).toExist();
   });
+
+  describe('render', () => {
+    it('should render Pause button when running', () => {
+      let controls = TestUtils.renderIntoDocument(<Controls countdownStatus="running" />),
+          $el      = $(ReactDOM.findDOMNode(controls)),
+          $pause   = $el.find('button:contains(Pause)'),
+          $start   = $el.find('button:contains(Start)');
+
+        expect($pause.length).toBe(1);  // Pause
+        expect($start.length).toBe(0);  // no Start
+    });
+
+    it('should render Start button when paused', () => {
+      let controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused" />),
+          $el      = $(ReactDOM.findDOMNode(controls)),
+          $pause   = $el.find('button:contains(Pause)'),
+          $start   = $el.find('button:contains(Start)');
+
+        expect($pause.length).toBe(0);    // no Pause
+        expect($start.length).toBe(1);    // Start
+    });
+  });
 });
