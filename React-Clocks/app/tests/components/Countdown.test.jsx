a40b754d0c19bbbc922b0a563a91760d98eae417
Julian Nicholls <juliannicholls29@gmail.com>
2016-05-15 19:38:09 +0100

    Test Countdown status changes

--- React-Clocks/app/tests/components/Countdown.test.jsx
+++ React-Clocks/app/tests/components/Countdown.test.jsx
@@ -36,5 +36,31 @@ describe('Countdown', () => {
         done();
       }, 2001);
     });
+
+    it('should pause countdown on paused status', (done) => {
+      let countdown = TestUtils.renderIntoDocument(<Countdown />);
+
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused')
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    it('should stop countdown on stopped status', (done) => {
+      let countdown = TestUtils.renderIntoDocument(<Countdown />);
+
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped')
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1001);
+    });
   });
 });
