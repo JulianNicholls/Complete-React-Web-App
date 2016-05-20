7cad36d86f97f32313cb71fcaf045d9319edd33e
Julian Nicholls <juliannicholls29@gmail.com>
2016-05-20 16:26:29 +0100

    Add and test other three actions

--- Todo-Redux/app/actions/actions.jsx
+++ Todo-Redux/app/actions/actions.jsx
@@ -1,3 +1,15 @@
 export var setSearchText = (searchText) => {
   return { type: 'SET_SEARCH_TEXT', searchText };
 };
+
+export var toggleShowCompleted = () => {
+  return { type: 'TOGGLE_SHOW_COMPLETED' };
+};
+
+export var addTask = (text) => {
+  return { type: 'ADD_TASK', text };
+};
+
+export var toggleTask = (id) => {
+  return { type: 'TOGGLE_TASK', id };
+};
--- Todo-Redux/app/tests/actions/actions.test.jsx
+++ Todo-Redux/app/tests/actions/actions.test.jsx
@@ -1,11 +1,32 @@
 import expect from 'expect';
 
-import {setSearchText} from 'actions';
+import {setSearchText, toggleShowCompleted, addTask, toggleTask} from 'actions';
 
 describe('Actions', () => {
   it('should generate search text action', () => {
     let action = { type: 'SET_SEARCH_TEXT', searchText: 'dog' },
         resp   = setSearchText('dog');
+
+    expect(resp).toEqual(action);
+  });
+
+  it('should generate toggle showCompleted action', () => {
+    let action = { type: 'TOGGLE_SHOW_COMPLETED' },
+        resp   = toggleShowCompleted();
+
+    expect(resp).toEqual(action);
+  });
+
+  it('should generate add task action', () => {
+    let action = { type: 'ADD_TASK', text: 'Walk the dog' },
+        resp   = addTask('Walk the dog');
+
+    expect(resp).toEqual(action);
+  });
+
+  it('should generate toggle task action', () => {
+    let action = { type: 'TOGGLE_TASK', id: 753 },
+        resp   = toggleTask(753);
 
     expect(resp).toEqual(action);
   });
