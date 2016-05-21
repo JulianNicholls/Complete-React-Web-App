7621bb7a65b4b2ad411d506dc219dabb6a1b2d5d
Julian Nicholls <juliannicholls29@gmail.com>
2016-05-21 18:52:20 +0100

    Add initialState to store configure

--- Todo-Redux/app/store/configureStore.jsx
+++ Todo-Redux/app/store/configureStore.jsx
@@ -1,14 +1,14 @@
 import {combineReducers, createStore, compose}                 from 'redux';
 import {searchTextReducer, showCompletedReducer, tasksReducer} from 'reducers';
 
-export var configure = () => {
+export var configure = (initialState = {}) => {
   var reducer = combineReducers({
     searchText:     searchTextReducer,
     showCompleted:  showCompletedReducer,
     tasks:          tasksReducer
   });
 
-  var store = createStore(reducer, compose(
+  var store = createStore(reducer, initialState, compose(
     window.devToolsExtension ? window.devToolsExtension() : f => f
   ));
 
