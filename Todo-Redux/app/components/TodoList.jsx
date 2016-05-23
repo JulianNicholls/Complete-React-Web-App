4d65fee423257046a78bc707a630deccc1f4e565
Julian Nicholls <juliannicholls29@gmail.com>
2016-05-23 12:15:05 +0100

    Update TodoList to filter for itself.

--- Todo-Redux/app/components/TodoList.jsx
+++ Todo-Redux/app/components/TodoList.jsx
@@ -3,32 +3,24 @@ import {connect} from 'react-redux';
 
 import TodoTask  from 'TodoTask';
 
+import TodoAPI   from 'TodoAPI';
+
 export var TodoList = React.createClass({
   render: function () {
-    let {tasks} = this.props;
+    let {searchText, showCompleted, tasks} = this.props;
 
     const renderTodos = _ => {
       if(tasks.length === 0) {
-        return (
-          <p className="container__message">There's nothing on your agenda</p>
-        );
+        return <p className="container__message">There's nothing on your agenda</p>;
       }
 
-      return tasks.map((task) => {
-        return (
-          <TodoTask key={task.id} {...task} />
-        );
+      return TodoAPI.filterTasks(tasks, showCompleted, searchText).map((task) => {
+        return <TodoTask key={task.id} {...task} />;
       });
     };
 
-    return (
-      <div>
-        {renderTodos()}
-      </div>
-    )
+    return <div>{renderTodos()}</div>;
   }
 });
 
-export default connect((state) => {
-  return { tasks: state.tasks }
-})(TodoList);
+export default connect((state) => state)(TodoList);
