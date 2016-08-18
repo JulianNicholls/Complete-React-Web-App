1ad09fb68cfb91d8d497761dad1f01a8995644be
Julian Nicholls <juliannicholls29@gmail.com>
2016-08-18 21:09:36 +0100

    Add resolving of paths via index.html to server.js
    This supports the use of browserHistory, rather than hashHistory

--- Todo-Redux-Firebase/server.js
+++ Todo-Redux-Firebase/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path    = require('path');
 
 // Create our app
 const app   = express();
@@ -14,6 +15,10 @@ app.use((req, resp, next) => {
 });
 
 app.use(express.static('public'));
+
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
 
 app.listen(PORT, _ => {
   console.log("Express loaded at port " + PORT);
